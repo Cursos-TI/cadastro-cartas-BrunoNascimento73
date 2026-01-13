40e88d308a99456fcb88812f07a0a8fae62f42c7
BrunoNascimento73 <202601126962@alunos.estacio.br>
2026-01-13 02:14:14 -0300

    Commit inicial adicionado entradas de dados 1 e 2 e exibição de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,87 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
+int main()
+{
   // Área para definição das variáveis para armazenar as propriedades das cidades
+  char estado1;     // Uma letra de 'A' a 'H' (representando um dos oito estados).
+  char codigo1[20]; // A letra do estado seguida de um número de 01 a 04 (ex: A01, B03)
+  char cidade1[20]; // O nome da cidade.
+  int pop1;         // O número de habitantes da cidade
+  float area1;      // A área da cidade em quilômetros quadrados.
+  float pib1;       // A área da cidade em quilômetros quadrados.
+  int turismo1;     // A quantidade de pontos turísticos na cidade.
+
+  char estado2;     // Uma letra de 'A' a 'H' (representando um dos oito estados).
+  char codigo2[20]; // A letra do estado seguida de um número de 01 a 04 (ex: A01, B03)
+  char cidade2[20]; // O nome da cidade.
+  int pop2;         // O número de habitantes da cidade
+  float area2;      // A área da cidade em quilômetros quadrados.
+  float pib2;       // A área da cidade em quilômetros quadrados.
+  int turismo2;     // A quantidade de pontos turísticos na cidade.
 
   // Área para entrada de dados
+  printf("diga o estado: \n");
+  scanf("%c", &estado1);
+
+  printf("diga o codigo: ");
+  scanf("%s", codigo1);
+
+  printf("diga a cidade: ");
+  scanf("%s", cidade1);
+
+  printf("diga o população: \n");
+  scanf("%d", &pop1);
+
+  printf("diga a area: \n");
+  scanf("%f", area1);
+
+  printf("diga a pib: ");
+  scanf("%f", pib1);
+
+  printf("diga a turismo: ");
+  scanf("%d", turismo1);
+
+  // entrada 2
+
+  printf("diga o estado: \n");
+  scanf("%c", &estado2);
+
+  printf("diga o codigo: ");
+  scanf("%s", codigo2);
+
+  printf("diga a cidade: ");
+  scanf("%s", cidade2);
+
+  printf("diga o população: \n");
+  scanf("%d", &pop2);
+
+  printf("diga a area: ");
+  scanf("%f", area2);
+
+  printf("diga a pib: ");
+  scanf("%f", pib2);
+
+  printf("diga a turismo: ");
+  scanf("%d", turismo2);
 
   // Área para exibição dos dados da cidade
+  // saida 1
+  printf("o estado é: %c\n", estado1);
+  printf("o codigo é: %s\n", codigo1);
+  printf("a cidade é %s \n", cidade1);
+  printf("a população é %d \n", pop1);
+  printf("a area é %f \n", area1);
+  printf("este é o pib %f \n", pib1);
+  printf("este é o turismo %d \n", turismo1);
+  // saida 2
+  printf("o estado é: %c\n", estado2);
+  printf("o codigo é: %s\n", codigo2);
+  printf("a cidade é %s \n", cidade2);
+  printf("a população é %d \n", pop2);
+  printf("a area é %f \n", area2);
+  printf("este é o pib %f \n", pib2);
+  printf("este é o turismo %d \n", turismo2);
 
-return 0;
-} 
+  return 0;
+}
